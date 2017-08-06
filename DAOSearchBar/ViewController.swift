87bc995c0c99077c8d71e5945725d673a51f7fd0
daoseng33 <lc.ray2011@gmail.com>
2017-08-06 16:22:29 +0800

    Initial Commit

---
+++ DAOSearchBar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DAOSearchBar
+//
+//  Created by Ray on 2017/8/6.
+//  Copyright © 2017年 Ray. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
