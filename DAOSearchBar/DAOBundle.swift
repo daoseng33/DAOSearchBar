d0b0d9fd8a65b21f34e4d14648a10a64b70b903e
ray <daoseng33@gmail.com>
2018-07-28 20:20:33 +0800

    [modify] Put images in bundle

---
+++ DAOSearchBar/DAOBundle.swift
@@ -0,0 +1,21 @@
+//
+//  DAOBundle.swift
+//  DAOSearchBarDemo
+//
+//  Created by Ray Dan on 2018/7/28.
+//  Copyright © 2018年 likeabossapp. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class DAOBundle {
+    class func podBundleImage(named: String) -> UIImage? {
+        let podBundle = Bundle(for: DAOBundle.self)
+        if let url = podBundle.url(forResource: "DAOSearchBar", withExtension: "bundle") {
+            let bundle = Bundle(url: url)
+            return UIImage(named: named, in: bundle, compatibleWith: nil)!
+        }
+        return nil
+    }
+}
--- DAOSearchBar/DAOSearchBar.swift
+++ DAOSearchBar/DAOSearchBar.swift
@@ -240,31 +240,30 @@ public class DAOSearchBar : UIView, UITextFieldDelegate, UIGestureRecognizerDele
         
         self.searchFrame.addSubview(searchImageViewOnContainerView)
         
-        let bundle = Bundle(for: DAOSearchBar.self)
         self.searchImageViewOn.frame = searchImageViewOnContainerView.bounds
         self.searchImageViewOn.alpha = 0.0
-        self.searchImageViewOn.image = UIImage(named: "NavBarIconSearch", in: bundle, compatibleWith: nil)
+        self.searchImageViewOn.image = DAOBundle.podBundleImage(named: "NavBarIconSearch")
         self.searchImageViewOn.tintColor = self.searchOnColor
         
         searchImageViewOnContainerView.addSubview(self.searchImageViewOn)
         
         self.searchImageCircle.frame = CGRect(x: 0.0, y: 0.0, width: 18.0, height: 18.0)
         self.searchImageCircle.alpha = 0.0
-        self.searchImageCircle.image = UIImage(named: "NavBarIconSearchCircle", in: bundle, compatibleWith: nil)
+        self.searchImageCircle.image = DAOBundle.podBundleImage(named: "NavBarIconSearchCircle")
         self.searchImageCircle.tintColor = self.searchOnColor
         
         searchImageViewOnContainerView.addSubview(self.searchImageCircle)
         
         self.searchImageCrossLeft.frame = CGRect(x: 14.0, y: 14.0, width: 8.0, height: 8.0)
         self.searchImageCrossLeft.alpha = 0.0
-        self.searchImageCrossLeft.image = UIImage(named: "NavBarIconSearchBar", in: bundle, compatibleWith: nil)
+        self.searchImageCrossLeft.image = DAOBundle.podBundleImage(named: "NavBarIconSearchBar")
         self.searchImageCrossLeft.tintColor = self.searchOnColor
         
         searchImageViewOnContainerView.addSubview(self.searchImageCrossLeft)
         
         self.searchImageCrossRight.frame = CGRect(x: 7.0, y: 7.0, width: 8.0, height: 8.0)
         self.searchImageCrossRight.alpha = 0.0
-        self.searchImageCrossRight.image = UIImage(named: "NavBarIconSearchBar2", in: bundle, compatibleWith: nil)
+        self.searchImageCrossRight.image = DAOBundle.podBundleImage(named: "NavBarIconSearchBar2")
         self.searchImageCrossRight.tintColor = self.searchOnColor
         
         searchImageViewOnContainerView.addSubview(self.searchImageCrossRight)
@@ -272,7 +271,7 @@ public class DAOSearchBar : UIView, UITextFieldDelegate, UIGestureRecognizerDele
         self.searchImageViewOff.frame = CGRect(x: self.bounds.width - kDAOSearchBarInset - kDAOSearchBarImageSize, y: (self.bounds.height - kDAOSearchBarImageSize) / 2, width: kDAOSearchBarImageSize, height: kDAOSearchBarImageSize)
         self.searchImageViewOff.autoresizingMask = [.flexibleLeftMargin, .flexibleTopMargin, .flexibleBottomMargin]
         self.searchImageViewOff.alpha = 1.0
-        self.searchImageViewOff.image = UIImage(named: "NavBarIconSearch", in: bundle, compatibleWith: nil)
+        self.searchImageViewOff.image = DAOBundle.podBundleImage(named: "NavBarIconSearch")
         self.searchImageViewOff.tintColor = self.searchOffColor
         
         self.searchFrame.addSubview(self.searchImageViewOff)
